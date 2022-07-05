534377f2d9a816137db7181b317775353d9bba1b
grigory baluev <gr.baluev@gmail.com>
2022-07-05 23:42:28 +0300

    add door_tile argument

--- gblock/apt_verificator/verificator.py
+++ gblock/apt_verificator/verificator.py
@@ -19,7 +19,7 @@ def check_corridor_states(layout, two_tiles_only=True):
 def get_space_type(layout, i, j):
     return layout.matrix.cells[i][j].spaceType
 
-def neighbours_indeces(i, j, layout):
+def neighbours_indeces(i, j, layout, apt):
     shape = layout.matrix.shape()
     ns = {'u': [i, j + 1],
           'r': [i + 1, j],
@@ -44,25 +44,48 @@ def neighbours_indeces(i, j, layout):
         res.add(str(k[0]) + ',' + str(k[1]))
     return res
 
-def check_corridor_neighbourhood(layout):
+def check_corridor_neighbourhood(layout, door_tile=False):
     apartments = layout.get_apartments()
-    n_indeces = set()
-
-
-    for apt in apartments:
-        n_indeces = set()
-        cells_flattened = apt.active_cells()
-        for cell_coords in cells_flattened:
-            i, j = cell_coords
-            for n_coords in neighbours_indeces(i, j, layout):
+    if not door_tile:
+        for apt in apartments:
+            n_indeces = set()
+            cells_flattened = [i.index(glob=True) for i in apt.active_cells()]
+            for cell_coords in cells_flattened:
+                i, j = cell_coords
+                for n_coords in neighbours_indeces(i, j, layout, apt):
+                    n_indeces.add(n_coords)
+            cells_coords_set = set()
+            for cell in cells_flattened:
+                cells_coords_set.add(str(cell[0]) + ',' + str(cell[1]))
+            cells_to_search = [tuple(int(j) for j in i.split(',')) for i in n_indeces.difference(cells_coords_set)]
+            neighbours_space_types = [get_space_type(layout, i, j) for (i, j) in cells_to_search]
+            if 'corridor' not in neighbours_space_types:
+                return False
+    else:
+        # test for layout #6
+        door_tile_dict = {0: (0, 1), 
+                          1: (0, 0), 
+                          2: (0, 0), 
+                          3: (0, 0), 
+                          4: (0, 0), 
+                          5: (0, 0), 
+                          6: (0, 2), 
+                          7: (0, 0), 
+                          8: (0, 0)}
+        for apt_index, apt in enumerate(apartments):
+            dt_i, dt_j = door_tile_dict[apt_index]
+            i, j = apt.cells[dt_i][dt_j].index(glob=True)
+            n_indeces = set()
+            for n_coords in neighbours_indeces(i, j, layout, apt):
                 n_indeces.add(n_coords)
-        cells_coords_set = set()
-        for cell in cells_flattened:
-            cells_coords_set.add(str(cell[0]) + ',' + str(cell[1]))
-        cells_to_search = [tuple(int(j) for j in i.split(',')) for i in n_indeces.difference(cells_coords_set)]
-        neighbours_space_types = [get_space_type(layout, i, j) for (i, j) in cells_to_search]
-        if 'corridor' not in neighbours_space_types:
-            return False
+            cells_coords_set = set()
+            cells_flattened = [i.pos for i in apt.active_cells()]
+            for cell in cells_flattened:
+                cells_coords_set.add(str(cell[0]) + ',' + str(cell[1]))
+            cells_to_search = [tuple(int(j) for j in i.split(',')) for i in n_indeces.difference(cells_coords_set)]
+            neighbours_space_types = [get_space_type(layout, i, j) for (i, j) in cells_to_search]
+            if 'corridor' not in neighbours_space_types:
+                return False
     return True
 
 
